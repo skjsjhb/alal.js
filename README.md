3ae1e9d780f9ebcef5bf5c7df658a7ea2f7fce34
Ted "skjsjhb" Gao <skjsjhb@outlook.com>
2023-11-16 21:52:12 +0800

    init(*): re-init commit
    - Forked repo and update all files.
    - Reformat all files.
    - Removed Boticorn, Cadance, DisplayManager.
    - Updated partial version info.
    - Fixed MS logout causing main process to crash.
    - Updated gitignore.

--- README.md
+++ README.md
@@ -1,63 +1,32 @@
-# Alicorn Launcher JS Edition
+# Alicorn Launcher Again (JS Edition)
 
-[中文 README](./README_ZH.md)
+## This Is a Fork
 
-## IMPORTANT
+The story has ended, really, but I'll light up everything again.
 
-As I, the developer, decided not to contribute to the Minecraft communities, this project **won't receive major updates** since Core 50 (that's why I call it Sunset).
+This is a fork of a the original [Alicorn](https://github.com/Andy-K-Sparklight/Alicorn) repository. See the original README there. The name **ALAL** stands for **AL**icorn **A**gain **L**auncher. 
 
-If servere security vulnerbilities are found in the future, patches might still be available. However, generally, this project is now discontinued.
+## Disclaimer
 
-It doesn't mean you have to switch to another launcher, but if you wish to, we recommand [HMCL](https://github.com/huanghongxun/HMCL), it's a free software (as in freedom) and is still being actively maintained.
+**ALAL is not compatible with Alicorn Legacy.** The data file format is updated and containers have changed a lot. No backward compatibility is guaranteed.
 
----
+Many packages of the original author is not possible to update in a few commits, they might get updated soon to avoid vulnerabilities, but with no warranty.
 
-A third party Minecraft launcher, with high performance and freedom.
+The fork is made to keep the software alive, not a follow-up or series. The updates are made out of good intentions, but still, with no warranty.
 
-![.](https://img.shields.io/badge/Alicorn-is%20cute!-df307f)
-![.](https://github.com/Andy-K-Sparklight/Alicorn/actions/workflows/codeql-analysis.yml/badge.svg)
-![.](https://github.com/Andy-K-Sparklight/Alicorn/actions/workflows/node.js.yml/badge.svg)
-![.](https://deepscan.io/api/teams/16407/projects/19670/branches/514338/badge/grade.svg)
-![.](https://img.shields.io/github/repo-size/Andy-K-Sparklight/Alicorn)
-![.](https://img.shields.io/github/license/Andy-K-Sparklight/Alicorn)
+## What To Update
 
-## Why Yet Another Launcher?
+- **Add Tests**: The original repository contains no tests.
 
-It's simple: I've been using different launchers and none of them can meet my requirements.
+- **Remove Unused Modules**: Many unused features should be discarded.
 
-And that's why we develop Alicorn.
+- **Fix Bugs**: There are several bugs with the latest game. They should be fixed.
 
-## Why Electron?
+- **Improve Download Speed**: Add an aria2 daemon as a faster download alternative.
 
-Still simple: I fancy it!
+- **Improve UI**: The UI needs to be clearer.
 
-Some other reasons include awesomely spectacular speed, etc.
-
-There's no need to consider size. After all, nothing is bigger than your OS ;)
-
-## Principles
-
-0. Free as in freedom.
-
-1. Code quality and bug fixes.
-
-2. Even weight on functionalities and performance.
-
-3. Windows last.
-
-4. Bash first.
-
-5. Size is not that important, but sometimes is important.
-
-6. Throw away those stereotypes, the runnable is the best.
-
-7. Embrace UTF-8.
-
-8. Line Feed only.
-
-9. Try to make Alicorn looks the same in different platforms, but not definitely.
-
-10. No SaaS.
+- **Support macOS**: We're restarting to macOS.
 
 ## Build
 
--- src/modules/container/MinecraftContainer.ts
+++ src/modules/container/MinecraftContainer.ts
@@ -3,130 +3,132 @@ import { getNativeArtifact, JAR_SUFFIX } from "../launch/NativesLint";
 import { LibraryMeta } from "../profile/Meta";
 
 export class MinecraftContainer {
-  id = "";
-  rootDir = "";
-  private readonly assetsRoot;
-  private readonly librariesBase;
-  private readonly versionsBase;
-  private readonly modsRoot;
-  private readonly resourcePacksRoot;
-  private readonly shaderPacksRoot;
-  private readonly savesRoot;
-  private readonly log4j2Root;
-  private readonly tempFileRoot;
-  private readonly dynamicModsRoot;
+    id = "";
+    rootDir = "";
+    private readonly assetsRoot;
+    private readonly librariesBase;
+    private readonly versionsBase;
+    private readonly modsRoot;
+    private readonly resourcePacksRoot;
+    private readonly shaderPacksRoot;
+    private readonly savesRoot;
+    private readonly log4j2Root;
+    private readonly tempFileRoot;
+    private readonly dynamicModsRoot;
 
-  getModJar(modJar: string): string {
-    return path.resolve(this.getModsRoot(), modJar);
-  }
+    constructor(rootDir: string, id: string) {
+        this.id = id;
+        this.rootDir = rootDir;
+        this.assetsRoot = this.resolvePath("assets");
+        this.librariesBase = this.resolvePath("libraries");
+        this.versionsBase = this.resolvePath("versions");
+        this.modsRoot = this.resolvePath("mods");
+        this.resourcePacksRoot = this.resolvePath("resourcepacks");
+        this.shaderPacksRoot = this.resolvePath("shaderpacks");
+        this.savesRoot = this.resolvePath("saves");
+        this.log4j2Root = this.resolvePath("log4j2-xml");
+        this.tempFileRoot = this.resolvePath("alicorn-temp");
+        this.dynamicModsRoot = this.resolvePath("alicorn-mods-dyn");
+    }
 
-  getVersionBase(): string {
-    return path.resolve(this.versionsBase);
-  }
+    getModJar(modJar: string): string {
+        return path.resolve(this.getModsRoot(), modJar);
+    }
 
-  getAssetsRootLegacy(): string {
-    return path.resolve(this.assetsRoot, "legacy");
-  }
-  getPff2LockFile(): string {
-    return path.resolve(this.rootDir, "pff2.lock");
-  }
-  getTempFileStorePath(relativePath: string): string {
-    return path.resolve(this.tempFileRoot, relativePath);
-  }
+    getVersionBase(): string {
+        return path.resolve(this.versionsBase);
+    }
 
-  getNativeLibraryExtractedRoot(library: LibraryMeta): string {
-    const nativeLibraryPath = getNativeArtifact(library).path;
-    return path.resolve(
-      this.getLibraryPath(
-        path.join(
-          path.dirname(nativeLibraryPath),
-          path.basename(nativeLibraryPath, JAR_SUFFIX)
-        )
-      )
-    );
-  }
+    getAssetsRootLegacy(): string {
+        return path.resolve(this.assetsRoot, "legacy");
+    }
 
-  getLog4j2FilePath(xmlName: string): string {
-    return path.resolve(this.log4j2Root, xmlName);
-  }
+    getPff2LockFile(): string {
+        return path.resolve(this.rootDir, "pff2.lock");
+    }
 
-  getAssetsIndexPath(index: string): string {
-    return path.resolve(this.assetsRoot, "indexes", index + ".json");
-  }
+    getTempFileStorePath(relativePath: string): string {
+        return path.resolve(this.tempFileRoot, relativePath);
+    }
 
-  getAssetPathLegacy(name: string): string {
-    return path.resolve(this.assetsRoot, "legacy", name);
-  }
+    getNativeLibraryExtractedRoot(library: LibraryMeta): string {
+        const nativeLibraryPath = getNativeArtifact(library).path;
+        return path.resolve(
+            this.getLibraryPath(
+                path.join(
+                    path.dirname(nativeLibraryPath),
+                    path.basename(nativeLibraryPath, JAR_SUFFIX)
+                )
+            )
+        );
+    }
 
-  getAssetPath(hash: string): string {
-    const header = hash.slice(0, 2);
-    return path.resolve(this.assetsRoot, "objects", header, hash);
-  }
+    getLog4j2FilePath(xmlName: string): string {
+        return path.resolve(this.log4j2Root, xmlName);
+    }
 
-  getSavesRoot(): string {
-    return this.savesRoot;
-  }
+    getAssetsIndexPath(index: string): string {
+        return path.resolve(this.assetsRoot, "indexes", index + ".json");
+    }
 
-  getResourcePacksRoot(): string {
-    return this.resourcePacksRoot;
-  }
+    getAssetPathLegacy(name: string): string {
+        return path.resolve(this.assetsRoot, "legacy", name);
+    }
 
-  getShaderPacksRoot(): string {
-    return this.shaderPacksRoot;
-  }
+    getAssetPath(hash: string): string {
+        const header = hash.slice(0, 2);
+        return path.resolve(this.assetsRoot, "objects", header, hash);
+    }
 
-  getModsRoot(): string {
-    return this.modsRoot;
-  }
+    getSavesRoot(): string {
+        return this.savesRoot;
+    }
 
-  getAssetsRoot(): string {
-    return this.assetsRoot;
-  }
+    getResourcePacksRoot(): string {
+        return this.resourcePacksRoot;
+    }
 
-  getVersionRoot(id: string): string {
-    return path.resolve(this.versionsBase, id);
-  }
+    getShaderPacksRoot(): string {
+        return this.shaderPacksRoot;
+    }
 
-  getProfilePath(id: string): string {
-    return path.resolve(this.getVersionRoot(id), id + ".json");
-  }
+    getModsRoot(): string {
+        return this.modsRoot;
+    }
 
-  getClientPath(id: string): string {
-    return path.resolve(this.getVersionRoot(id), id + ".jar");
-  }
+    getAssetsRoot(): string {
+        return this.assetsRoot;
+    }
 
-  getLibraryPath(libPath: string): string {
-    return path.resolve(this.librariesBase, libPath);
-  }
+    getVersionRoot(id: string): string {
+        return path.resolve(this.versionsBase, id);
+    }
 
-  getLibrariesRoot(): string {
-    return this.librariesBase;
-  }
+    getProfilePath(id: string): string {
+        return path.resolve(this.getVersionRoot(id), id + ".json");
+    }
 
-  resolvePath(relativePath?: string): string {
-    return path.resolve(this.rootDir, relativePath || "");
-  }
+    getClientPath(id: string): string {
+        return path.resolve(this.getVersionRoot(id), id + ".jar");
+    }
 
-  getDynamicModsRoot(): string {
-    return this.dynamicModsRoot;
-  }
+    getLibraryPath(libPath: string): string {
+        return path.resolve(this.librariesBase, libPath);
+    }
 
-  getDynamicModJar(modJar: string): string {
-    return path.resolve(this.getDynamicModsRoot(), modJar);
-  }
+    getLibrariesRoot(): string {
+        return this.librariesBase;
+    }
 
-  constructor(rootDir: string, id: string) {
-    this.id = id;
-    this.rootDir = rootDir;
-    this.assetsRoot = this.resolvePath("assets");
-    this.librariesBase = this.resolvePath("libraries");
-    this.versionsBase = this.resolvePath("versions");
-    this.modsRoot = this.resolvePath("mods");
-    this.resourcePacksRoot = this.resolvePath("resourcepacks");
-    this.shaderPacksRoot = this.resolvePath("shaderpacks");
-    this.savesRoot = this.resolvePath("saves");
-    this.log4j2Root = this.resolvePath("log4j2-xml");
-    this.tempFileRoot = this.resolvePath("alicorn-temp");
-    this.dynamicModsRoot = this.resolvePath("alicorn-mods-dyn");
-  }
+    resolvePath(relativePath?: string): string {
+        return path.resolve(this.rootDir, relativePath || "");
+    }
+
+    getDynamicModsRoot(): string {
+        return this.dynamicModsRoot;
+    }
+
+    getDynamicModJar(modJar: string): string {
+        return path.resolve(this.getDynamicModsRoot(), modJar);
+    }
 }

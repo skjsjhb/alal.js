598ff58e289b56f3c98b747f5e07a17b9742d68b
Ted "skjsjhb" Gao <skjsjhb@outlook.com>
2023-11-25 03:38:20 +0800

    style(auth): added ms login url to sources

--- src/modules/auth/MicrosoftBrowserLogin.ts
+++ src/modules/auth/MicrosoftBrowserLogin.ts
@@ -1,4 +1,5 @@
 import { WindowManager } from "@/background/WindowManager";
+import Sources from "@/constra/sources.json";
 import { BrowserWindow, dialog, Event, session } from "electron";
 import { Locale } from "../i18n/Locale";
 
@@ -8,10 +9,7 @@ import { Locale } from "../i18n/Locale";
  * Unlike most modules, this can only be called on the background.
  */
 export namespace MicrosoftBrowserLogin {
-    const loginURL =
-        "https://login.live.com/oauth20_authorize.srf?client_id=00000000402b5328&" +
-        "response_type=code&scope=service%3A%3Auser.auth.xboxlive.com%3A%3AMBI_SSL&" +
-        "redirect_uri=https%3A%2F%2Flogin.live.com%2Foauth20_desktop.srf";
+    const loginURL = Sources.microsoftLogin;
 
     const loginBrowserPart = "ms-login";
 

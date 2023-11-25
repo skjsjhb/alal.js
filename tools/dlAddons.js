6c51c5338e1f766e8dc907eb213872239133e059
Ted "skjsjhb" Gao <skjsjhb@outlook.com>
2023-11-26 00:32:00 +0800

    fix(ci): fixed missing directory for addon script

--- tools/dlAddons.js
+++ tools/dlAddons.js
@@ -1,7 +1,8 @@
 const os = require("os");
-const { copyFile, remove } = require("fs-extra");
+const { copyFile, remove, ensureDir } = require("fs-extra");
 const { createGlobalProxyAgent } = require("global-agent");
 let fetch;
+const path = require("path");
 const { pipeline } = require("stream/promises");
 const { createWriteStream } = require("fs");
 const decompress = require("decompress");
@@ -10,6 +11,7 @@ async function download(url, loc) {
     if (!fetch) {
         fetch = (await import("node-fetch")).default;
     }
+    await ensureDir(path.dirname(loc));
     const res = await fetch(url, {
         agent: createGlobalProxyAgent()
     });
@@ -23,6 +25,7 @@ async function dlAria2() {
         console.log("Downloading aria2 for Windows.");
         const aria2Url = "https://github.com/aria2/aria2/releases/download/release-1.37.0/aria2-1.37.0-win-64bit-build1.zip";
         await download(aria2Url, "build/addons/aria2-win32.zip");
+        console.log("Decompressing archive.");
         await decompress("build/addons/aria2-win32.zip", "build/addons/aria2-win32");
         await copyFile("build/addons/aria2-win32/aria2-1.37.0-win-64bit-build1/aria2c.exe", "build/addons/aria2c.exe");
         await remove("build/addons/aria2-win32");
@@ -33,6 +36,7 @@ async function dlAria2() {
         console.log("Downloading aria2 for macOS.");
         const aria2Url = "https://github.com/aria2/aria2/releases/download/release-1.35.0/aria2-1.35.0-osx-darwin.tar.bz2";
         await download(aria2Url, "build/addons/aria2-mac.tar.bz2");
+        console.log("Decompressing archive.");
         await decompress("build/addons/aria2-mac.tar.bz2", "build/addons/aria2-mac");
         await copyFile("build/addons/aria2-mac/aria2-1.35.0/bin/aria2c", "build/addons/aria2c");
         await remove("build/addons/aria2-mac");

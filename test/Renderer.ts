bde9009a9090fb460014e2826dd01475f4d61780
Ted "skjsjhb" Gao <skjsjhb@outlook.com>
2023-11-26 12:02:42 +0800

    fix(ci): added options override on ci

--- test/Renderer.ts
+++ test/Renderer.ts
@@ -2,6 +2,7 @@
  * Entry point of the new renderer for autotest bundles only.
  */
 import { Options } from "@/modules/data/Options";
+import { Paths } from "@/modules/data/Paths";
 import { ReInit } from "@/renderer/reinit/ReInit";
 import { ipcRenderer } from "electron";
 import os from "os";
@@ -20,12 +21,16 @@ async function main() {
         ipcRenderer.send(SignalTest.LOG_ERR, ...args);
     };
 
-
-    await ReInit.initRenderer();
+    Paths.detectRootPath();
+    await Paths.retrieveAppPath();
     Options.get().dev = true;
     if (os.platform() != "win32") {
         Options.get().download.aria2.enabled = false; // Unstable
     }
+    await Options.save();
+
+    await ReInit.initRenderer();
+
 
     // Autotest modules entry
     console.warn("This is a test bundle built for automated tests. They are NOT intended for normal use.");

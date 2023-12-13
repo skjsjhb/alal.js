e08d39ff3e9243459a946af13bbd47806cead7da
Ted "skjsjhb" Gao <skjsjhb@outlook.com>
2023-12-14 02:59:53 +0800

    feat(net): extend proxy support
    - Extend proxy support on *nix systems by using proxy-agent.

--- src/modules/net/ProxyMan.ts
+++ src/modules/net/ProxyMan.ts
@@ -1,13 +1,26 @@
 import { MAPI } from '@/background/MAPI';
+import { OSType } from '@/modules/util/OSType';
 import { ipcRenderer } from 'electron';
 import { HttpsProxyAgent } from 'https-proxy-agent';
+import { Agent } from 'node:http';
+import { ProxyAgent } from 'proxy-agent';
 
-export async function getProxyAgent(url: string): Promise<HttpsProxyAgent<string> | undefined> {
-    const proxyConfig = (await ipcRenderer.invoke(MAPI.GET_PROXY, url)) as string;
-    if (proxyConfig.startsWith('DIRECT')) {
-        return undefined;
+let proxyAgent: ProxyAgent;
+
+export async function getProxyAgent(url: string): Promise<Agent | undefined> {
+    const sys = OSType.self();
+
+    if (sys.isWindows()) {
+        // On windows, Electron picks the proxy for us
+        const proxyConfig = (await ipcRenderer.invoke(MAPI.GET_PROXY, url)) as string;
+        if (proxyConfig.startsWith('DIRECT')) {
+            return undefined;
+        } else {
+            const host = proxyConfig.replaceAll('PROXY', '').trim();
+            return new HttpsProxyAgent('http://' + host);
+        }
     } else {
-        const host = proxyConfig.replaceAll('PROXY', '').trim();
-        return new HttpsProxyAgent('http://' + host);
+        // Environment variables are set
+        return proxyAgent ?? (proxyAgent = new ProxyAgent());
     }
 }
